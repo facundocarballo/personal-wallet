586397875a6d30decdba88a41ef5baa0feca52ba
Facundo Carballo <carballofacundo70@gmail.com>
2024-03-16 13:41:24 -0300

    tag controllers

---
+++ infrastructure/controllers/tag.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const Create = async (req: Request, res: Response): Promise<void> => {};
+
+export const Get = async (req: Request, res: Response): Promise<void> => {};
+
+export const Update = async (req: Request, res: Response): Promise<void> => {};
+
+export const Delete = async (req: Request, res: Response): Promise<void> => {};
--- infrastructure/routes/tag.ts
+++ infrastructure/routes/tag.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { CheckJWT } from "../../handlers/jwt";
+import { Create, Delete, Get, Update } from "../controllers/tag";
 
 export const TagRouter = express.Router();
 
-TagRouter.get("/", CheckJWT);
-TagRouter.post("/", CheckJWT);
-TagRouter.put("/", CheckJWT);
-TagRouter.delete("/", CheckJWT);
+TagRouter.get("/", CheckJWT, Get);
+TagRouter.post("/", CheckJWT, Create);
+TagRouter.put("/", CheckJWT, Update);
+TagRouter.delete("/", CheckJWT, Delete);
