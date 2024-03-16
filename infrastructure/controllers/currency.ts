17445da814b3639187ec2ec1efb2dd7919e5f663
Facundo Carballo <carballofacundo70@gmail.com>
2024-03-16 13:53:50 -0300

    currency hexagonal architecture

--- infrastructure/controllers/currency.ts
+++ infrastructure/controllers/currency.ts
@@ -1,4 +1,9 @@
 import { Request, Response } from "express";
+import { MySqlCurrencyRepository } from "../../adapters/currency/mysql";
+import { CurrencyService } from "../../services/currency";
+
+const repository = new MySqlCurrencyRepository();
+const service = new CurrencyService(repository);
 
 export const Create = async (req: Request, res: Response): Promise<void> => {
   const userId = (req as any).user.id;
