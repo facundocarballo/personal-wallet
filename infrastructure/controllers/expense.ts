205a6c766f4f214547b776c688deb9bcc43567e0
Facundo Carballo <carballofacundo70@gmail.com>
2024-03-20 12:27:01 -0300

    income and expense - controllers

--- infrastructure/controllers/expense.ts
+++ infrastructure/controllers/expense.ts
@@ -1,13 +1,47 @@
 import { Request, Response } from "express";
 import { MySqlExpenseRepository } from "../../adapters/expense/mysql";
 import { ExpenseService } from "../../services/expense";
+import { Expense } from "../../domain/expense";
+import ErrorMessages from "../../errors.json";
 
 const repository = new MySqlExpenseRepository();
 const service = new ExpenseService(repository);
 
-export const Create = async (req: Request, res: Response): Promise<void> => {};
+export const Create = async (req: Request, res: Response): Promise<void> => {
+  const expense = Expense.bodyToIncome(req.body);
+  if (!expense) {
+    res.status(400).send(ErrorMessages.income.post[400]);
+    return;
+  }
 
-export const Get = async (req: Request, res: Response): Promise<void> => {};
+  const expenseCreated = await service.Create(expense);
+  if (!expenseCreated) {
+    res.status(500).send(ErrorMessages[500]);
+    return;
+  }
+
+  res.status(200).send({
+    income: expenseCreated,
+  });
+};
+
+export const Get = async (req: Request, res: Response): Promise<void> => {
+  const user_id = (req as any).user_id;
+  if (!user_id) {
+    res.status(400).send(ErrorMessages.income.get[400]);
+    return;
+  }
+
+  const incomes = await service.Get(user_id);
+  if (!incomes) {
+    res.status(500).send(ErrorMessages[500]);
+    return;
+  }
+
+  res.status(200).send({
+    incomes: incomes,
+  });
+};
 
 export const Delete = async (req: Request, res: Response): Promise<void> => {};
 
--- infrastructure/controllers/income.ts
+++ infrastructure/controllers/income.ts
@@ -1,13 +1,47 @@
 import { Request, Response } from "express";
 import { MySqlIncomeRepository } from "../../adapters/income/mysql";
 import { IncomeService } from "../../services/income";
+import { Income } from "../../domain/income";
+import ErrorMessages from "../../errors.json";
 
 const repository = new MySqlIncomeRepository();
 const service = new IncomeService(repository);
 
-export const Create = async (req: Request, res: Response): Promise<void> => {};
+export const Create = async (req: Request, res: Response): Promise<void> => {
+  const income = Income.bodyToIncome(req.body);
+  if (!income) {
+    res.status(400).send(ErrorMessages.income.post[400]);
+    return;
+  }
 
-export const Get = async (req: Request, res: Response): Promise<void> => {};
+  const incomeCreated = await service.Create(income);
+  if (!incomeCreated) {
+    res.status(500).send(ErrorMessages[500]);
+    return;
+  }
+
+  res.status(200).send({
+    income: incomeCreated,
+  });
+};
+
+export const Get = async (req: Request, res: Response): Promise<void> => {
+  const user_id = (req as any).user_id;
+  if (!user_id) {
+    res.status(400).send(ErrorMessages.income.get[400]);
+    return;
+  }
+
+  const incomes = await service.Get(user_id);
+  if (!incomes) {
+    res.status(500).send(ErrorMessages[500]);
+    return;
+  }
+
+  res.status(200).send({
+    incomes: incomes,
+  });
+};
 
 export const Delete = async (req: Request, res: Response): Promise<void> => {};
 
