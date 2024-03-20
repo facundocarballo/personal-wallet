3d1bf736a7b5c78d4b1df2ee821fa1c3475d7e68
Facundo Carballo <carballofacundo70@gmail.com>
2024-03-20 12:39:10 -0300

    income and expense - mysql repository

--- adapters/expense/mysql.ts
+++ adapters/expense/mysql.ts
@@ -1,12 +1,39 @@
 import { Expense } from "../../domain/expense";
+import {
+  ExecuteCreateMySqlStoredProcedure,
+  ExecuteGetMySqlStoredProcedure,
+} from "../../handlers/stored_procedures";
 import { ExpenseRepository } from "../../ports/expense";
 
 export class MySqlExpenseRepository implements ExpenseRepository {
-  Create(transaction: Expense): Promise<Expense | undefined> {
-    throw new Error("Method not implemented.");
+  async Create(transaction: Expense): Promise<Expense | undefined> {
+    const expenseId = await ExecuteCreateMySqlStoredProcedure(
+      "CALL CreateExpense(?, ?, ?, ?, @expense_id)",
+      "SELECT @expense_id AS new_id",
+      [
+        transaction.account_id,
+        transaction.category_id,
+        transaction.tag_id,
+        transaction.amount,
+      ]
+    );
+    if (!expenseId) return undefined;
+    return new Expense(
+      expenseId,
+      transaction.account_id,
+      transaction.category_id,
+      transaction.tag_id,
+      transaction.amount,
+      transaction.timestamp
+    );
   }
-  Get(user_id: number): Promise<Expense[] | undefined> {
-    throw new Error("Method not implemented.");
+  async Get(user_id: number): Promise<Expense[] | undefined> {
+    const expenses = await ExecuteGetMySqlStoredProcedure(
+      "CALL GetExpenses(?)",
+      [user_id]
+    );
+    if (!expenses) return undefined;
+    return Expense.anyToArray(expenses);
   }
   Update(transaction: Expense): Promise<Expense | undefined> {
     throw new Error("Method not implemented.");
--- adapters/income/mysql.ts
+++ adapters/income/mysql.ts
@@ -1,12 +1,32 @@
 import { Income } from "../../domain/income";
+import {
+  ExecuteCreateMySqlStoredProcedure,
+  ExecuteGetMySqlStoredProcedure,
+} from "../../handlers/stored_procedures";
 import { IncomeRepository } from "../../ports/income";
 
 export class MySqlIncomeRepository implements IncomeRepository {
-  Create(transaction: Income): Promise<Income | undefined> {
-    throw new Error("Method not implemented.");
+  async Create(transaction: Income): Promise<Income | undefined> {
+    const incomeId = await ExecuteCreateMySqlStoredProcedure(
+      "CALL CreateIncome(?, ?, ?, @income_id)",
+      "SELECT @income_id AS new_id",
+      [transaction.account_id, transaction.category_id, transaction.amount]
+    );
+    if (!incomeId) return undefined;
+    return new Income(
+      incomeId,
+      transaction.account_id,
+      transaction.category_id,
+      transaction.amount,
+      transaction.timestamp
+    );
   }
-  Get(user_id: number): Promise<Income[] | undefined> {
-    throw new Error("Method not implemented.");
+  async Get(user_id: number): Promise<Income[] | undefined> {
+    const incomes = await ExecuteGetMySqlStoredProcedure("CALL GetIncomes(?)", [
+      user_id,
+    ]);
+    if (!incomes) return undefined;
+    return Income.anyToArray(incomes);
   }
   Update(transaction: Income): Promise<Income | undefined> {
     throw new Error("Method not implemented.");
